5c93e629eafd919d16b2f02cb27b39a87a8ae18b
qudroturkey <reesemb1@gmail.com>
2020-04-10 11:39:51 -0500

    it works!

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
+
+const API = ('https://boiling-brook-94902.herokuapp.com/transactions')
 
 class AccountContainer extends Component {
 
   constructor() {
     super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+    this.state = {
+      transactions: [],
+      searchTerm: "",
+      results: []
+    }
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    this.fetchTransactions()
   }
+
+  fetchTransactions = () => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => this.setState({transactions: data}))
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+    this.filterResults()
+  }
+
+  filterResults = () => {
+    let filtered = this.state.transactions.slice(0)
+    filtered = filtered.filter(i => i.description.toLowerCase().includes(this.state.searchTerm) || i.category.toLowerCase().includes(this.state.searchTerm))
+    this.setState({
+      results: filtered
+    })
+  }
+
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        { this.state.searchTerm.length > 0 ? <TransactionsList t={this.state.results} /> : <TransactionsList t={this.state.transactions}/> }
+        {console.log(this.state.searchTerm)}
       </div>
     )
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +29,8 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {props.t.map(i => <Transaction key={i.id} t={i} />)}
+        {/* {"... your code here..."} */}
 
       </tbody>
     </table>
