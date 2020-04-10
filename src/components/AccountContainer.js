fbf920a1fb231cb15a0baf91fa2eaacdd24b1dea
jbshipman <jbshipman@icloud.com>
2020-04-10 11:52:26 -0500

    commit to eval on

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import { transactions } from '../transactionsData'
+
+const API_URL = 'https://boiling-brook-94902.herokuapp.com/transactions'
 
 class AccountContainer extends Component {
 
   constructor() {
     super()
+    this.state = {
+      ledger: [],
+      newLedger: []
+    };
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
-  }
+  };
 
-  handleChange(event) {
-    // your code here
-  }
+  componentDidMount() {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(ledger => this.setState({ ledger }))
+
+  };
+
+  handleChange = (letter) => {
+    let newLedger = this.state.ledger.filter(item => (item.description.match(letter) || (item.category.match(letter))))
+    this.setState({ newLedger })
+
+    // how do I get it to seach case insensitive?
+
+  };
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList ledger={!this.state.newLedger.length ? this.state.ledger : this.state.newLedger} />
       </div>
     )
   }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{props.lineItem.posted_at}</td>
+      <td>{props.lineItem.description}</td>
+      <td>{props.lineItem.category}</td>
+      <td>{props.lineItem.amount}</td>
     </tr>
   )
 }
