ea57a4672796f3c580d5a233a8c0fc46bd0fa444
jessicatriana <jessica.triana.tx@gmail.com>
2020-04-10 11:46:25 -0500

    Submission

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,70 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+// import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-
   constructor() {
-    super()
+    super();
+
+    this.state = {
+      transactions: [],
+      searchWord: "",
+    };
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    const URL = "https://boiling-brook-94902.herokuapp.com/transactions";
+
+    fetch(URL)
+      .then((response) => response.json())
+      .then((transactions) => {
+        this.setState({ transactions });
+      });
   }
+
+  handleChange = (event) => {
+    console.log("Event Target Value is", event.target.value);
+    this.setState({
+      searchWord: event.target.value,
+    });
+  };
 
   render() {
-
+    // console.log(this.state.transactions);
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search
+          searchWord={this.state.searchWord}
+          handleChange={this.handleChange}
+        />
+        <TransactionsList
+          searchWord={this.state.searchWord}
+          transactions={this.state.transactions.filter(
+            (
+              line // Should have used a Switch statement to capture all possibilities
+            ) =>
+              line.description.includes(
+                this.state.searchWord.charAt(0).toUpperCase() +
+                  this.state.searchWord.slice(1) || //capitalizes first letter of searchWord
+                  this.state.searchWord  // accepts exactly what was entered
+              ) ||
+              line.category.includes(
+                this.state.searchWord.charAt(0).toUpperCase() +
+                  this.state.searchWord.slice(1) || //capitalizes first letter of searchWord
+                  this.state.searchWord || // accepts exactly what was entered
+                  this.state.searchWord.toLowerCase ||
+                  this.state.searchWord.toUpperCase
+              )
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
